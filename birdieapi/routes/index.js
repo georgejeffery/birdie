fe9215acdd205a7645f5cc8e9ee3c11ec1d726ef
georgejeffery <georgejeffery@gmail.com>
2019-05-08 16:08:14 +0100

    Second commit

--- birdieapi/routes/index.js
+++ birdieapi/routes/index.js
@@ -1,9 +1,26 @@
-var express = require('express');
+var express = require("express");
+var _ = require("lodash");
 var router = express.Router();
 
+var output = [];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  res.locals.connection.query(`describe census_learn_sql`, function(
+    error,
+    results,
+    fields
+  ) {
+    if (error) {
+      res.send(JSON.stringify({ status: 500, error: error, response: null }));
+    } else {
+      console.log(results);
+      for (var i in results) {
+        output.push(_.values(results[i])[0]);
+      }
+      res.send(JSON.stringify({ columns: output }));
+    }
+  });
 });
 
 module.exports = router;
--- birdieapi/routes/people.js
+++ birdieapi/routes/people.js
@@ -2,12 +2,15 @@ var express = require("express");
 var _ = require("lodash");
 var router = express.Router();
 
-var option = "'class of worker'";
-var sql = `SELECT ${option}, age from census_learn_sql LIMIT 0, 5`;
-var countarray = [];
-var age = 0;
+var option = "`class of worker`";
+var sql = `SELECT ${option}, age from census_learn_sql`;
 
-router.get("/", function(req, res, next) {
+router.get("/:query", function(req, res, next) {
+  var countarray = [];
+  var age = 0;
+
+  var option = `\`` + `${decodeURI(req.params.query)}` + `\``;
+  var sql = `SELECT ${option}, age from census_learn_sql`;
   res.locals.connection.query(sql, function(error, results, fields) {
     if (error) {
       res.send(JSON.stringify({ status: 500, error: error, response: null }));
@@ -16,13 +19,27 @@ router.get("/", function(req, res, next) {
         countarray.push(_.values(results[i])[0]);
         age += results[i].age;
       }
-      console.log(results.first);
-      console.log(age);
-      console.log(results.length);
-      ave_age = age / results.length;
-      output = _.countBy(countarray);
-      console.log(_.countBy(countarray));
-      res.send(JSON.stringify({ average_age: ave_age, sorted: output }));
+      var ave_age = age / results.length;
+      var output = _.countBy(countarray);
+      var list = _.uniq(countarray);
+      var bb = {};
+      var out = 0;
+      for (var x in list) {
+        var section = '"' + `${list[x]}` + '"';
+        q = `SELECT AVG(age) as ${section} FROM census_learn_sql WHERE ${option} = ${section}`;
+        //console.log(x);
+        res.locals.connection.query(q, function(error, result, fields) {
+          if (error) {
+            console.log(error);
+          } else {
+            name = _.keys(result[0])[0];
+            out = _.values(_.values(result)[0])[0];
+            console.log(name, out);
+            bb[name] = out;
+          }
+        });
+      }
+      res.send(JSON.stringify({ average_age: bb, sorted: output }));
     }
   });
 });
